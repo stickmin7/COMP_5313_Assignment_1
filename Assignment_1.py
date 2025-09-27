81cf3ae4bcf6db16ae16887b98c64f4e65f15cc2
Henry <hamaaha@lakeheadu.ca>
2025-09-27 16:26:33 -0400

    cleaned CNN structure

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,7 +1,7 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras import layers, models
 from tensorflow.keras.optimizers import Adam
 
@@ -13,54 +13,88 @@ class convolutional_network():
 
         self.dataset = RetinaMNIST(split="test", download=True, size=64) # command from https://medmnist.com/ documentation
 
-        self.train_images = np.load('/home/henry/.medmnist/retinamnist_64.npz')['train_images']
+        self.train_images = np.array(tf.image.rgb_to_grayscale(np.load('/home/henry/.medmnist/retinamnist_64.npz')['train_images'])/255)
         self.train_labels = np.load('/home/henry/.medmnist/retinamnist_64.npz')['train_labels']
         self.train_labels = tf.keras.utils.to_categorical(self.train_labels, num_classes=5)
-        self.val_images = np.load('/home/henry/.medmnist/retinamnist_64.npz')['val_images']
+        self.val_images = np.array(tf.image.rgb_to_grayscale(np.load('/home/henry/.medmnist/retinamnist_64.npz')['val_images'])/255)
         self.val_labels = np.load('/home/henry/.medmnist/retinamnist_64.npz')['val_labels']
         self.val_labels = tf.keras.utils.to_categorical(self.val_labels, num_classes=5)
-        self.test_images = np.load('/home/henry/.medmnist/retinamnist_64.npz')['test_images']
+        self.test_images = np.array(tf.image.rgb_to_grayscale(np.load('/home/henry/.medmnist/retinamnist_64.npz')['test_images'])/255)
         self.test_labels = np.load('/home/henry/.medmnist/retinamnist_64.npz')['test_labels']
         self.test_labels = tf.keras.utils.to_categorical(self.test_labels, num_classes=5)
 
-        self.learning_rate = 0.0001
-        self.loss_function = 'categorical_crossentropy'
-        self.epoch = 50
-        self.batch_size = 16
+        self.learning_rate = 0.00001
+        self.loss_function = 'CategoricalCrossentropy'
+        self.epoch = 1500
+        self.batch_size = 128
 
-    def input_layer(self):
+        self.model = tf.keras.Sequential()
 
-        return layers.Input(shape=(64, 64, 3)) 
+    def view_label_distribution(self):
 
-    def convolution_layers(self, layer, kernels, dim):
-        
-        return layers.Conv2D(kernels, dim, activation='relu', padding='same')(layer)
+        print("Training data distribution: ")
+        print(np.unique(np.load('/home/henry/.medmnist/retinamnist_64.npz')['train_labels'], return_counts=True))
+        print("Validation data distribution: ")
+        print(np.unique(np.load('/home/henry/.medmnist/retinamnist_64.npz')['val_labels'], return_counts=True))
+        print("Testing data distribution: ")
+        print(np.unique(np.load('/home/henry/.medmnist/retinamnist_64.npz')['test_labels'], return_counts=True))
 
-    def max_pooling_layers(self, layer, pool_x, pool_y):
+    def define_model(self):
 
-        return layers.MaxPooling2D(pool_size=(pool_x, pool_y), padding='same')(layer)
-    
-    def dropout_layers(self, layer):
-        
-        return layers.Dropout(0.4)(layer)
-    
-    def batch_normalization(self, layer):
+        self.model.add(layers.Input(shape=(64, 64, 1)))
 
-        return layers.BatchNormalization()(layer)
+        self.model.add(layers.Conv2D(32, (3, 3,), activation='leaky_relu', padding='same'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-    def dense_layers(self, layer, neurons, activation_func):
-        
-        return layers.Dense(neurons, activation=activation_func)(layer)        
+        self.model.add(layers.Conv2D(64, (3, 3,), activation='leaky_relu', padding='same'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-    def train_CNN_model(self, input_layer, output_layer):
+        #self.model.add(layers.Conv2D(128, (3, 3,), activation='leaky_relu', padding='same'))
+        #self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))    
 
-        model = models.Model(input_layer, output_layer)
+        self.model.add(layers.Flatten())
 
-        model.compile(optimizer=Adam(learning_rate=self.learning_rate), loss=self.loss_function, metrics=['acc'])
+        self.model.add(layers.Dense(128, 'leaky_relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.5))
 
-        model.summary()
+        self.model.add(layers.Dense(64, 'leaky_relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.5))
 
-        return model.fit(self.train_images, self.train_labels, validation_data = (self.val_images, self.val_labels), epochs=self.epoch, batch_size=self.batch_size, verbose=2)
+        self.model.add(layers.Dense(32, 'leaky_relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.5))     
+
+        self.model.add(layers.Dense(5, 'softmax'))
+
+    def train_CNN_model(self):
+
+        self.model.compile(optimizer=Adam(learning_rate=self.learning_rate), loss=self.loss_function, metrics=['acc'])
+
+        self.model.summary()
+
+        print(self.train_images.shape)
+        print(self.train_images.shape)
+
+        return self.model.fit(self.train_images, self.train_labels, validation_data = (self.val_images, self.val_labels), epochs=self.epoch, batch_size=self.batch_size, verbose=2)
+
+    def plot_learning_curves(self, training_accuracy, training_loss, validation_accuracy, validation_loss):
+
+        plt.plot(range(0, self.epoch, 1), training_accuracy, label="training_accuracy")
+        plt.plot(range(0, self.epoch, 1), validation_accuracy, label="validation_accuracy")
+
+        plt.title("Accuracy learning curves")
+        plt.show()
+
+        plt.plot(range(0, self.epoch, 1), training_loss, label="training_loss")
+        plt.plot(range(0, self.epoch, 1), validation_loss, label="validation_loss")
+
+        plt.title("Loss Learning curves")
+        plt.show()
 
 def main():
     
@@ -68,37 +102,22 @@ def main():
 
     classification_network = convolutional_network()
 
+    # View label distribution for training, validation, testing
+
+    classification_network.view_label_distribution()
+
     # define the model
 
-    input_layer = classification_network.input_layer()
-    
-    # convolutions and max poolings
+    classification_network.define_model()
 
-    conv_layer = classification_network.convolution_layers(input_layer, 64, 5)
-    normalization = classification_network.batch_normalization(conv_layer)
-    conv_layer = classification_network.convolution_layers(normalization, 128, 3)
-    normalization = classification_network.batch_normalization(conv_layer)
-    max_pooling = classification_network.max_pooling_layers(normalization, 2, 2)
+    trained_CNN = classification_network.train_CNN_model()
 
-    # Convert 2D matrices into 1D feature matrix
+    training_loss = trained_CNN.history['loss']
+    validation_loss = trained_CNN.history['val_loss']
+    training_accuracy = trained_CNN.history['acc']
+    validation_accuracy = trained_CNN.history['val_acc']
 
-    flattened_layer = layers.Flatten()(max_pooling)
-
-    # Dense layers
-
-    dense_layer = classification_network.dense_layers(flattened_layer, 512, 'sigmoid')
-    dropout = classification_network.dropout_layers(dense_layer)
-    dense_layer = classification_network.dense_layers(dropout, 256, 'sigmoid')
-    dropout = classification_network.dropout_layers(dense_layer)
-    dense_layer = classification_network.dense_layers(dropout, 128, 'sigmoid')
-    dropout = classification_network.dropout_layers(dense_layer)
-    output_layer = classification_network.dense_layers(dropout, 5, 'sigmoid')
-
-    # Encoded data to fix input shape error
-
-    trained_CNN = classification_network.train_CNN_model(input_layer, output_layer)
-
-    print(trained_CNN.history)
+    classification_network.plot_learning_curves(training_accuracy, training_loss, validation_accuracy, validation_loss)
 
     print("model_finished")
 
